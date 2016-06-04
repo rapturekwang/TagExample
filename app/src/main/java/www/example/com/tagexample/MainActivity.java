b8e1cd1e19eddf9e012ad8aec3b04527bc931428
kwang <rapturekwang@gmail.com>
2016-06-04 11:37:46 +0900

    version2

--- app/src/main/java/www/example/com/tagexample/MainActivity.java
+++ app/src/main/java/www/example/com/tagexample/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = (TextView) findViewById(R.id.textview);
-        textView.setText("version 1");
+        textView.setText("version 2");
     }
 }
